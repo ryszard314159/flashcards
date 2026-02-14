6821e59c0ad9048691d7e5c9534a7e4b08200236
Ryszard Czerminski <ryszard@czerminski.net>
2026-02-14 13:10:46 -0500

    version update added

--- script.js
+++ script.js
@@ -232,3 +232,18 @@ function speak(text, langCode) {
 
 // Pre-load voices for some browsers
 window.speechSynthesis.onvoiceschanged = () => synth.getVoices();
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js').then(reg => {
+        reg.addEventListener('updatefound', () => {
+            const newWorker = reg.installing;
+            newWorker.addEventListener('statechange', () => {
+                if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
+                    if (confirm("New version available! Refresh now?")) {
+                        window.location.reload();
+                    }
+                }
+            });
+        });
+    });
+}
--- sw.js
+++ sw.js
@@ -1,12 +1,29 @@
-const CACHE_NAME = 'flashcard-v1';
+const CACHE_NAME = 'flashcard-v2'; // Increment this version to force update
 const ASSETS = [
-  'index.html',
-  'manifest.json'
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icons/favicon.svg'
 ];
 
+// Install: Cache new assets
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting(); // Force the new service worker to become active immediately
+});
+
+// Activate: Clean up old caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
   );
 });
 
