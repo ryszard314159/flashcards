5b3d60992201eb71bdd73be94a832c4848197340
Ryszard Czerminski <ryszard@czerminski.net>
2026-02-15 09:16:08 -0500

    new version 2026-02-15.3

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-    VERSION: "2026-02-15.2",
+    VERSION: "2026-02-15.3",
     // You can add more here later, like:
     // DEFAULT_SPEED: 0.9,
     // DARK_MODE: true
--- sw.js
+++ sw.js
@@ -1,28 +1,47 @@
-// sw.js
 import { CONFIG } from './config.js';
-const CACHE_NAME = CONFIG.VERSION;
 
-const ASSETS = ['./', './index.html', './style.css', './script.js', './config.js', './manifest.json'];
+const CACHE_NAME = CONFIG.VERSION; 
 
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './config.js',
+  './manifest.json',
+  './icons/favicon.svg'
+];
+
+// INSTALL: Pre-cache assets
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('activate', (e) => {
+  // force move to 'active' state even if old SW exists
+  self.skipWaiting(); 
   e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
-    }).then(() => self.clients.claim())
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// CACHE-FIRST (This allows 'updatefound' to trigger)
+// ACTIVATE: Clean up old versions and take control
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    }).then(() => self.clients.claim()) // Important: Take control of open tabs immediately
+  );
+});
+
+// FETCH: Cache-first
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
 
+// MESSAGE: Trigger the update
 self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
