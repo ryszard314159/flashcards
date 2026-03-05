e015af61cbd114aa5c65991a9189eaac6954ae83
Ryszard Czerminski <ryszard@czerminski.net>
2026-03-05 09:20:31 -0500

    new version

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 // config.js
 // 
 export const CONFIG = {
-    VERSION: "2026-03-04",
+    VERSION: "2026-03-05.0",
     // You can add more here later, like:
     // DEFAULT_SPEED: 0.9,
     // DARK_MODE: true
