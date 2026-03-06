86fb241b993edc4845ea583c0b97e744e5a1a074
Ryszard Czerminski <ryszard@czerminski.net>
2026-03-06 18:58:59 -0500

    added Vamos

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //
 // sw.js - Service Worker for Flashcards App
 //
-// VERSION: 2026-03-06.1833
+// VERSION: 2026-03-06.1858
 import { CONFIG } from './src/config.js';
 
 const CACHE_NAME = CONFIG.VERSION; 
