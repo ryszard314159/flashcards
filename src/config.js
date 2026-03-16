bc9c4ea74f59d5270afcc99181acbd94b4ed725e
Ryszard Czerminski <ryszard@czerminski.net>
2026-03-16 19:06:41 -0400

    VERSION: 2026-03-16.1906

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ export const DEBUG = true;
  * General application configuration.
  */
 export const CONFIG = {
-    VERSION: "2026-03-15.2126",
+    VERSION: "2026-03-16.1906",
     // You can add more here later, like:
     // DEFAULT_SPEED: 0.9,
     // DARK_MODE: true
